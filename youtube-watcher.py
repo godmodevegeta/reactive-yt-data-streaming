a4cb85e4c2077d1f89a2d4c561225326c21e4cb4
godmodevegeta <kshubham1904@gmail.com>
2025-06-14 09:59:14 +0530

    Refactor fetch_playlist_info to utilize page_token for pagination and update logging level

--- youtube-watcher.py
+++ youtube-watcher.py
@@ -17,18 +17,26 @@ def fetch_playlist_page(api_key, playlist_id, page_token=None):
     
 
 
-# def fetch_playlist_info(api_key, playlist_id, page_token=None):
-#     fetch_playlist_page(api_key, playlist_id, page_token)
-#     logging.debug(response.text)
+def fetch_playlist_info(api_key, playlist_id, page_token=None):
+    page = fetch_playlist_page(api_key, playlist_id, page_token)
+    
+    yield from page["items"]
+    next_page_token = page.get("nextPageToken")
+    logging.info("found nextagetoken: %s", next_page_token)
+    if next_page_token:
+        yield from fetch_playlist_info(api_key, playlist_id, next_page_token)
+
+
 
 def main():
     logging.info("Hello, there!")
     api_key = config["google_api_key"]
     playlist_id = config["playlist_id"]
     logging.info("found key: %s", api_key)
-    fetch_playlist_page(api_key, playlist_id, "EAAaHlBUOkNBVWlFREl4UkRKQk5ETXlORU0zTXpKQk16SQ")
+    for video_item in fetch_playlist_info(api_key, playlist_id, ):
+        logging.info(video_item)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     sys.exit(main())
